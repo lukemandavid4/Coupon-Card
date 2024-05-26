e5a4e82694eee00b033f141842637a463bc54bde
lukemandavid4 <lukemandavid4@gmail.com>
2024-05-26 23:44:46 +0300

    updated

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import image from '../src/assets/uber.png'
+const App = () => {
+  return (
+    <div className='container'>
+      <div className="container__image">
+        <img src={image} alt="image" />
+      </div>
+      <div className="container__description">
+        <span>20% flat off on all rides within the city</span>
+        <span>Using HDFC Credit Card</span>
+      </div>
+      <div className="container__coupon">
+        <span>AA-784-421</span>
+        <span>COPY CODE</span>
+      </div>
+      <div className="container__valid">
+        <span>Valid Till: 20 Dec 2027</span>
+      </div>
+    </div>
+  )
+}
+
+export default App
